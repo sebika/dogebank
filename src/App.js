88cbbef42bf4852626599afef3f6f8ed44e3c4df
sebika <sebymarinescu@yahoo.ca>
2021-04-16 09:53:50 +0300

    Connect to web firebase app API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
 
 function App() {
+  const firebaseApp = firebase.apps[0];
   return (
     <div className="App">
+
+      <h1>React & Firebase</h1>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDx9yELRjmSCtwHrEcRUhOKXob_2Od8vgY",
+  authDomain: "dogebank-connect.firebaseapp.com",
+  projectId: "dogebank-connect",
+  storageBucket: "dogebank-connect.appspot.com",
+  messagingSenderId: "225936319423",
+  appId: "1:225936319423:web:c2dab13abefb1322a8370a",
+  measurementId: "G-57H96PV20F"
+});
 
 ReactDOM.render(
   <React.StrictMode>
