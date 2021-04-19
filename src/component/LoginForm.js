9864d9eb0f6b6f5f6107659271aa64b9bf91fe2a
sebikaLegion <sebymarinescu@yahoo.ca>
2021-04-19 13:56:00 +0200

    Fix newlines and quotes

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -8,36 +8,36 @@ export function LoginForm() {
       <h3>Sign In</h3>
       <br></br>
 
-      <div className="form-group">
-        <label className="ml-3">Email</label>
+      <div className='form-group'>
+        <label className='ml-3'>Email</label>
         <div className='input-group'>
           <div className='input-group-addon mt-2 mr-1'>
             <AiOutlineMail />
           </div>
-          <input type="email" className="form-control" placeholder="Enter email" />
+          <input type='email' className='form-control' placeholder='Enter email' />
         </div>
       </div>
 
-      <div className="form-group">
-        <label className="ml-3">Password</label>
+      <div className='form-group'>
+        <label className='ml-3'>Password</label>
         <div className='input-group'>
           <div className='input-group-addon mt-2 mr-1'>
             <RiLockPasswordLine />
           </div>
-          <input type="password" className="form-control" placeholder="Enter password" />
+          <input type='password' className='form-control' placeholder='Enter password' />
         </div>
       </div>
 
-      <div className="form-group">
-          <div className="custom-control custom-checkbox">
-              <input type="checkbox" className="custom-control-input" id="customCheck1" />
-              <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+      <div className='form-group'>
+          <div className='custom-control custom-checkbox'>
+              <input type='checkbox' className='custom-control-input' id='customCheck1' />
+              <label className='custom-control-label' htmlFor='customCheck1'>Remember me</label>
           </div>
       </div>
 
-      <button type="submit" className="btn btn-primary btn-block">Submit</button>
-      <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
+      <button type='submit' className='btn btn-primary btn-block'>Submit</button>
+      <p className='forgot-password text-right'>
+          Forgot <a href='#'>password?</a>
       </p>
   </form>
   );
