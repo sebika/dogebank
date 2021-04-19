d6369f8423643c506ddfc8c7468b370fd0b7b1ae
sebikaLegion <sebymarinescu@yahoo.ca>
2021-04-19 14:00:04 +0200

    Make login form a little bit wider

--- src/component/LoginPage.js
+++ src/component/LoginPage.js
@@ -4,7 +4,7 @@ export function LoginPage() {
   return (
     <div className='row mt-5'>
       <div className='col'></div>
-        <div className='col-3'>
+        <div className='col-5'>
             <LoginForm />
         </div>
       <div className='col'></div>
