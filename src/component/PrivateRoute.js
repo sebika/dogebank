38aae464c5881d7fc2053c6850890caf8a03239e
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2021-05-09 00:43:07 +0300

    Use custom check in private route

--- src/component/PrivateRoute.js
+++ src/component/PrivateRoute.js
@@ -3,14 +3,14 @@ import { Route, Redirect } from 'react-router-dom'
 
 import { useAuth } from '../contexts/AuthContext'
 
-export function PrivateRoute({ component: Component, ...rest }) {
+export function PrivateRoute({ component: Component, check, ...rest }) {
   const { currentUser } = useAuth()
 
   return (
     <Route
       {...rest}
       render={props => {
-        return currentUser ? <Component {...props} /> : <Redirect to='/' />
+        return currentUser && (!check || check(currentUser)) ? <Component {...props} /> : <Redirect to='/' />
       }}
     ></Route>
   )
