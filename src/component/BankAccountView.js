ead6027845f28447c7be8013672532c814267b9d
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2021-05-09 11:18:27 +0300

    Add operation buttons

--- src/component/BankAccountView.js
+++ src/component/BankAccountView.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { FiMoreVertical } from 'react-icons/fi'
+import { Card, Button, ListGroup, ListGroupItem, Dropdown, DropdownButton, ButtonGroup } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
 
 import { useAuth } from '../contexts/AuthContext'
@@ -28,12 +29,31 @@ export function BankAccountView() {
     return (
       <>
         <Card border='secondary' className='mt-3'>
-          <Card.Header>{props.doc.get('nume')}</Card.Header>
+          <Card.Header>
+            <div style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}>
+              {props.doc.get('nume')}
+              <Dropdown as={ButtonGroup} >
+                <Dropdown.Toggle variant='link' bsPrefix='p-0'>
+                  <FiMoreVertical size='24' />
+                </Dropdown.Toggle>
+                <Dropdown.Menu align='right'>
+                  <Dropdown.Item eventKey='1' >Create Transaction</Dropdown.Item>
+                  <Dropdown.Item eventKey='2'>View Transactions</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+          </Card.Header>
           <Card.Body>
             <ListGroup className='list-group-flush'>
               <ListGroupItem><strong>IBAN: </strong> {props.doc.get('IBAN')}</ListGroupItem>
               <ListGroupItem><strong>Sold: </strong> {props.doc.get('suma')} {props.doc.get('moneda')}</ListGroupItem>
             </ListGroup>
+
+            <Button variant='btn btn-danger ml-3 float-right'>Close Account</Button>
           </Card.Body>
         </Card>
       </>
