14bc42c85c2fcfea4104ac200add231d05f46f8c
sebika <sebymarinescu@yahoo.ca>
2021-05-10 15:11:11 +0300

    Remove unnecessary import

--- src/component/UpdateProfile.js
+++ src/component/UpdateProfile.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
-import { Link, useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import { useAuth } from '../contexts/AuthContext'
 import Client from '../models/Client'
