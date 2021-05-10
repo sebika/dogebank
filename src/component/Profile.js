14c84abad036382d47485dc3afe221cf8513af34
andreeadutulescu <55826493+andreeadutulescu@users.noreply.github.com>
2021-05-10 11:56:01 +0300

    View user information feature (#35)
    * View user information feature
    
    * Change styles & fix warnings
    
    * Fixed Go Back Button
    
    Co-authored-by: andreea <andreea99dutulescu@gmail>

---
+++ src/component/Profile.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Button, Card, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Link, useHistory } from 'react-router-dom'
+
+import { useAuth } from '../contexts/AuthContext'
+
+export function Profile() {
+  const { currentUser } = useAuth()
+  const history = useHistory()
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>My Profile</h2>
+
+          <ListGroup className='list-group-flush'>
+              <ListGroupItem><strong>First Name: </strong> {currentUser.db.get('prenume')}</ListGroupItem>
+              <ListGroupItem><strong>Second Name: </strong> {currentUser.db.get('nume')}</ListGroupItem>
+              <ListGroupItem><strong>Username: </strong> {currentUser.db.get('nume_utilizator')}</ListGroupItem>
+              <ListGroupItem><strong>Email: </strong> {currentUser.db.get('mail')}</ListGroupItem>
+              <ListGroupItem><strong>National ID: </strong> {currentUser.db.get('CNP')}</ListGroupItem>
+              <ListGroupItem><strong>Address: </strong> {currentUser.db.get('adresa')}</ListGroupItem>
+          </ListGroup>
+
+          <Link to='/update-profile'>
+            <Button className='w-100'>
+              Update Profile
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+
+      <div className='w-100 text-center mt-2' style={{marginBottom:70}}>
+        <Button variant='link' onClick={() => history.goBack()}>
+          Go Back
+        </Button>
+      </div>
+      
+    </>
+  )
+}
