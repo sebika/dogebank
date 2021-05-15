8e089df407cb939c22e792b462b0d7c0b5675d14
sebika <sebymarinescu@yahoo.ca>
2021-05-15 15:30:23 +0300

    Change information in database using Update Profile

--- src/component/views/UpdateProfileView.js
+++ src/component/views/UpdateProfileView.js
@@ -1,28 +1,54 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
+import { auth } from '../../firebase'
 
 import { useAuth } from '../../contexts/AuthContext'
 import Client from '../../models/Client'
 
 export function UpdateProfileView() {
+  const firstNameRef = useRef()
+  const lastNameRef = useRef()
+  const usernameRef = useRef()
   const emailRef = useRef()
+  const nationalIDRef = useRef()
+  const addressRef = useRef()
+
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  const { currentUser, updatePassword, updateEmail } = useAuth()
+  const { currentUser, updatePassword, updateEmail, setCurrentUser } = useAuth()
   const [error, setError] = useState('')
   const [loading, setLoading] = useState(false)
   const history = useHistory()
 
   async function handleSubmit(e) {
     e.preventDefault()
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+    if (passwordRef.current.value !== passwordConfirmRef.current.value)
       return setError('Error: Passwords do not match')
-    }
 
-    let sameEmailQuery = (await Client.all().where('mail', '==', emailRef.current.value).get())
-    if (sameEmailQuery.size !== 0 && emailRef.current.value !== currentUser.email)
+    const sameEmailQuery = await Client.all().where('mail', '==', emailRef.current.value).get()
+    if (sameEmailQuery.size !== 0 && emailRef.current.value !== currentUser.db.get('mail'))
       return setError('Error: This email is already used with another account')
+
+    const sameUsernameQuery = await Client.all().where('nume_utilizator', '==', usernameRef.current.value).get()
+    if (sameUsernameQuery.size !== 0 && usernameRef.current.value !== currentUser.db.get('nume_utilizator'))
+      return setError('Error: The username is already used with another account')
+
+    const sameIDQuery = await Client.all().where('CNP', '==', nationalIDRef.current.value).get()
+    if (sameIDQuery.size !== 0 && nationalIDRef.current.value !== currentUser.db.get('CNP'))
+      return setError('Error: The nationality ID is already used with another account')
+
+    if (nationalIDRef.current.value.length !== 13)
+      return setError('Your CNP does not have exactly 13 characters')
+
+    if (/^\d+$/.test(nationalIDRef.current.value) === false)
+      return setError('Your CNP contains non-digit characters')
+
+    if (addressRef.current.value.length <= 0)
+      return setError('You must fill in your address')
+
+    if (usernameRef.current.value.length < 6)
+      return setError('Your username must be at least 6 characters long')
 
     const promises = []
     setLoading(true)
@@ -37,9 +63,16 @@ export function UpdateProfileView() {
     Promise.all(promises)
       .then(async () => {
         if (emailRef.current.value) {
-          await Client.all().doc(currentUser.db.id).update({mail: emailRef.current.value})
+          await Client.all().doc(currentUser.db.id).update({
+            prenume: firstNameRef.current.value,
+            nume: lastNameRef.current.value,
+            nume_utilizator: usernameRef.current.value,
+            CNP: nationalIDRef.current.value,
+            adresa: addressRef.current.value,
+            mail: emailRef.current.value
+          })
         }
-        history.push('/dashboard')
+        history.push('/profile')
       })
       .catch((err) => {
         setError(`${err}`)
@@ -49,6 +82,14 @@ export function UpdateProfileView() {
       })
   }
 
+  useEffect(() => {
+    return auth.onAuthStateChanged(async user => {
+      if (user)
+        user.db = (await Client.all().where('mail', '==', user.email).get()).docs[0]
+      setCurrentUser(user)
+    })
+  })
+
   return (
     <>
       <Card>
@@ -56,17 +97,51 @@ export function UpdateProfileView() {
           <h2 className='text-center mb-4'>Update Profile</h2>
           {error && <Alert variant='danger'>{error}</Alert>}
           <Form onSubmit={handleSubmit}>
+            <Form.Group id='firstName'>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                ref={firstNameRef}
+                defaultValue={currentUser.db.get('prenume')}
+              />
+            </Form.Group>
+            <Form.Group id='lastName'>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                ref={lastNameRef}
+                defaultValue={currentUser.db.get('nume')}
+              />
+            </Form.Group>
+            <Form.Group id='username'>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                ref={usernameRef}
+                defaultValue={currentUser.db.get('nume_utilizator')}
+              />
+            </Form.Group>
+            <Form.Group id='nationalID'>
+              <Form.Label>National ID</Form.Label>
+              <Form.Control
+                ref={nationalIDRef}
+                defaultValue={currentUser.db.get('CNP')}
+              />
+            </Form.Group>
+            <Form.Group id='address'>
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                ref={addressRef}
+                defaultValue={currentUser.db.get('adresa')}
+              />
+            </Form.Group>
             <Form.Group id='email'>
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type='email'
                 ref={emailRef}
-                required
-                defaultValue={currentUser.email}
+                defaultValue={currentUser.db.get('mail')}
               />
             </Form.Group>
             <Form.Group id='password'>
-              <Form.Label>Password</Form.Label>
+              <Form.Label>New password</Form.Label>
               <Form.Control
                 type='password'
                 ref={passwordRef}
@@ -74,7 +149,7 @@ export function UpdateProfileView() {
               />
             </Form.Group>
             <Form.Group id='password-confirm'>
-              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Label>Retype new password</Form.Label>
               <Form.Control
                 type='password'
                 ref={passwordConfirmRef}
